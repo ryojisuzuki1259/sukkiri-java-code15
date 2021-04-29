02f2dbe1c7813233a60caf299da19177f6298e5f
suzukiryoji <suzukiryoji@localhost>
2021-04-30 03:13:40 +0900

    Write codes

--- src/code15/Main.java
+++ src/code15/Main.java
@@ -3,11 +3,11 @@ package code15;
 public class Main {
 
 	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < 1000; i++) {
-			sb.append("Java");
+		String s = "abc,def:ghi";
+		String[] words = s.split("[,:]");
+		for (String w : words) {
+			System.out.println(w + "->");
 		}
-		String s = sb.toString();
 
 	}
 
