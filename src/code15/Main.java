d047c0204567731e1a272ccd888d42a0ca0fecf3
suzukiryoji <suzukiryoji@localhost>
2021-04-30 08:20:03 +0900

    Write codes

--- src/code15/Main.java
+++ src/code15/Main.java
@@ -3,11 +3,9 @@ package code15;
 public class Main {
 
 	public static void main(String[] args) {
-		String s = "abc,def:ghi";
-		String[] words = s.split("[,:]");
-		for (String w : words) {
-			System.out.println(w + "->");
-		}
+		final String FORMAT = "%-9s %-13s 所持金%,6d";
+		String s = String.format(FORMAT, hero.getName(), hero.getJob(), hero.Gold());
+		System.out.println(s);
 
 	}
 
