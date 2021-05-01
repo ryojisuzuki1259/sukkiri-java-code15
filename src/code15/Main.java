d390439322bca3e01c3d486cb87efaee77250411
suzukiryoji <suzukiryoji@localhost>
2021-05-01 14:07:58 +0900

    Write codes

--- src/code15/Main.java
+++ src/code15/Main.java
@@ -1,12 +1,14 @@
 package code15;
+import java.util.Date;
 
 public class Main {
 
 	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		// 時間がかかる処理を記述
-		long end = System.currentTimeMillis();
-		System.out.println("処理にかかった時間は" + (end - start) + "ミリ秒でした");
+		Date now = new Date();
+		System.out.println(now);
+		System.out.println(now.getTime());
+		Date past = new Date(1600705425827L);
+		System.out.println(past);
 	}
 
 }
