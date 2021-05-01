da99cd33e52ee584dfd5ec17cedff59fb6f6558d
suzukiryoji <suzukiryoji@localhost>
2021-05-01 23:10:20 +0900

    Write codes

--- src/code15/Main.java
+++ src/code15/Main.java
@@ -1,21 +1,19 @@
 package code15;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Calendar c = Calendar.getInstance();
-		// ６つのint値からDateインスタンスを生成
-		c.set(2019, 8, 22, 1, 23, 45);
-		c.set(Calendar.MONTH, 9);
-		Date d = c.getTime();
+	public static void main(String[] args) throws Exception {
+		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		// 文字列からDateインスタンスを生成
+		Date d = f.parse("2020/09/22 01:23:45");
 		System.out.println(d);
-		// Dateインスタンスからint値を生成
+		// Dateインスタンスから文字列を生成
 		Date now = new Date();
-		c.setTime(now);
-		int y = c.get(Calendar.YEAR);
-		System.out.println("今年は" + y + "年です");
+		String s = f.format(now);
+		System.out.println("現在は" + s + "です");
+
 	}
 
 }
