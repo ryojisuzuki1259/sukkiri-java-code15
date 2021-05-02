bf69c7ac392fceddd325278a8ab4236ab4f579a4
suzukiryoji <suzukiryoji@localhost>
2021-05-02 14:02:59 +0900

    Write codes

--- src/code15/Main.java
+++ src/code15/Main.java
@@ -1,18 +1,23 @@
 package code15;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		// 文字列からDateインスタンスを生成
-		Date d = f.parse("2020/09/22 01:23:45");
-		System.out.println(d);
-		// Dateインスタンスから文字列を生成
-		Date now = new Date();
-		String s = f.format(now);
-		System.out.println("現在は" + s + "です");
+		// インスタンスの生成
+		Instant i1 = Instant.now();
+		// Instantとlong値との相互変換
+		Instant i2 = Instant.ofEpochMilli(1600705425827L);
+		long l = i2.toEpochMilli();
+
+		// ZonedDateTimeの生成
+		ZonedDateTime z1 = ZonedDateTime.now();
+		ZonedDateTime z2 = ZonedDateTime.of(2020, 1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo"));
+
+		// InstantとZonedDateTimeの相互変換
+
 
 	}
 
