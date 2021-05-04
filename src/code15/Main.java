aa2bf5e009603bd3c7910dd149a82c77bfadc66f
suzukiryoji <suzukiryoji@localhost>
2021-05-04 16:09:12 +0900

    Write codes

--- src/code15/Main.java
+++ src/code15/Main.java
@@ -1,30 +1,17 @@
 package code15;
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		// インスタンスの生成
-		Instant i1 = Instant.now();
-		// Instantとlong値との相互変換
-		Instant i2 = Instant.ofEpochMilli(1600705425827L);
-		long l = i2.toEpochMilli();
+	public static void main(String[] args) {
+		// LocalDateTimeの生成方法
+		LocalDateTime l1 = LocalDateTime.now();
+		LocalDateTime l2 = LocalDateTime.of(2020, 1, 1, 9, 5, 0, 0);
 
-		// ZonedDateTimeの生成
-		ZonedDateTime z1 = ZonedDateTime.now();
-		ZonedDateTime z2 = ZonedDateTime.of(2020, 1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo"));
-
-		// InstantとZonedDateTimeの相互変換
-		Instant i3 = z2.toInstant();
-		ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
-
-		// ZonedDateTimeの利用方法
-		System.out.println("東京:" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
-		System.out.println("ロンドン:" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
-		if (z2.isEqual(z3)) {
-			System.out.println("これらは同じ瞬間を指しています");
-		}
+		// LocalDateTimeとZonedDateTimeの相互変換
+		ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+		LocalDateTime l3 = z1.toLocalDateTime();
 	}
 }
